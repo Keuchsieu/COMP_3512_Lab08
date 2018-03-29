17aabca1a319abf31a4292dfeb614ad2c1c42b85
Yuqing Qiu <yuqingqiu93@gmail.com>
2018-03-28 20:49:29 -0700

    implemented add

--- COMP_3512_Lab08/FixedList.cpp
+++ COMP_3512_Lab08/FixedList.cpp
@@ -4,6 +4,8 @@ int main() {
 	FixedList<int, 5> l;
 	std::cout << l.capacity() << std::endl;
 	std::cout << l.size() << std::endl;
+	l.add(1);
+	std::cout << l.size() << std::endl;
 	system("pause");
 	return 0;
 }
--- COMP_3512_Lab08/FixedList.hpp
+++ COMP_3512_Lab08/FixedList.hpp
@@ -11,7 +11,7 @@ public:
 	//int getFirstIndex(const T& t) const;
 	size_t size() const;
 	size_t capacity() const;
-	//bool add(const T& t);
+	bool add(const T& t);
 	//T remove(const T& t);
 private:
 	std::list<T> l;
@@ -42,4 +42,14 @@ template<class T, size_t N>
 inline size_t FixedList<T, N>::capacity() const
 {
 	return N;
+}
+
+template<class T, size_t N>
+inline bool FixedList<T, N>::add(const T & t)
+{
+	if(s >= N)
+		return false;
+	l.push_back(t);
+	s++;
+	return true;
 }
