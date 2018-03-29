95c6a2031ad102faeb3eb6633357f9c48952d340
Yuqing Qiu <yuqingqiu93@gmail.com>
2018-03-28 20:41:42 -0700

    initialize with size implementation in header

---
+++ COMP_3512_Lab08/FixedList.cpp
@@ -0,0 +1,8 @@
+#include"FixedList.hpp"
+
+int main() {
+	FixedList<int, 5> l;
+	std::cout << l.size() << std::endl;
+	system("pause");
+	return 0;
+}
---
+++ COMP_3512_Lab08/FixedList.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include<list>
+#include<iostream>
+template<class T, size_t N>
+class FixedList {
+public:
+	FixedList();
+	//~FixedList();
+	//const T& get(unsigned int index) const;
+	//T& operator[](unsigned int index);
+	//int getFirstIndex(const T& t) const;
+	size_t size() const;
+	//size_t capacity() const;
+	//bool add(const T& t);
+	//T remove(const T& t);
+private:
+	std::list<T> l;
+	size_t s;
+};
+
+
+
+template<class T, size_t N>
+inline FixedList<T, N>::FixedList()
+{
+	l = std::list<T>(N);
+	s = N;
+}
+
+template<class T, size_t N>
+inline size_t FixedList<T, N>::size() const 
+{
+	return s;
+}
