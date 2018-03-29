9e6810521b76b4db874842cc83be6622c08e3cfb
Yuqing Qiu <yuqingqiu93@gmail.com>
2018-03-29 10:21:23 -0700

    why i used a list?

--- COMP_3512_Lab08/FixedList.cpp
+++ COMP_3512_Lab08/FixedList.cpp
@@ -4,8 +4,10 @@ int main() {
 	FixedList<int, 5> l;
 	std::cout << l.capacity() << std::endl;
 	std::cout << l.size() << std::endl;
-	l.add(1);
+	std::cout << std::boolalpha << "adding to list: " << l.add(1) << std::endl;
 	std::cout << l.size() << std::endl;
+	l.get(1);
+	//std::cout << l.get(1) << std::endl;
 	system("pause");
 	return 0;
 }
--- COMP_3512_Lab08/FixedList.hpp
+++ COMP_3512_Lab08/FixedList.hpp
@@ -1,55 +1,77 @@
 #pragma once
-#include<list>
 #include<iostream>
+
 template<class T, size_t N>
 class FixedList {
 public:
 	FixedList();
 	~FixedList();
-	//const T& get(unsigned int index) const;
+	const T& get(unsigned int index) const;
 	//T& operator[](unsigned int index);
 	//int getFirstIndex(const T& t) const;
 	size_t size() const;
 	size_t capacity() const;
 	bool add(const T& t);
-	//T remove(const T& t);
+	T remove(const T& t);
 private:
-	std::list<T> l;
+	T l[N];
 	size_t s;
 };
 
 
-
+/* default constructor */
 template<class T, size_t N>
 inline FixedList<T, N>::FixedList()
 {
-	l = std::list<T>(N);
 	s = 0;
 }
 
+/* default destructor */
 template<class T, size_t N>
 inline FixedList<T, N>::~FixedList()
 {
 }
 
+
+/* PRE: index inbound */
+template<class T, size_t N>
+inline const T & FixedList<T, N>::get(unsigned int index) const
+{
+	// TODO: insert return statement here
+	return l[index];
+}
+
+/* return how many elements are in the list */
 template<class T, size_t N>
 inline size_t FixedList<T, N>::size() const 
 {
 	return s;
 }
 
+/* return what is the maximum number of elements in this list */
 template<class T, size_t N>
 inline size_t FixedList<T, N>::capacity() const
 {
 	return N;
 }
 
+/* adds to the list
+   if addition success: return true
+   if addition failed: return false
+*/
 template<class T, size_t N>
 inline bool FixedList<T, N>::add(const T & t)
 {
 	if(s >= N)
 		return false;
-	l.push_back(t);
+	l[s] = t;
 	s++;
 	return true;
+}
+
+template<class T, size_t N>
+inline T FixedList<T, N>::remove(const T & t)
+{
+	s--;
+	return l[s+1];
 }
